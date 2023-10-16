efaae1b11156e246a8fc2e571dffdbd40502d5d2
huan <huansonecacntt@gmail.com>
2023-10-16 10:29:55 -0300

    first commit

---
+++ src/main/java/br/com/huansantos/todolist/task/TaskModel.java
@@ -0,0 +1,40 @@
+package br.com.huansantos.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Id;
+import lombok.Data;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+     private UUID id;
+     private String description;
+
+     @Column(length =  50)
+     private String title;
+     private LocalDateTime startAt;
+     private LocalDateTime endAt;
+     private String priority;
+     private UUID idUser;
+
+     @CreationTimestamp
+     private LocalDateTime createdAt;
+
+     public void setTitle(String title) throws Exception{
+          if(title.length() > 50){
+               throw new Exception("O campo title deve conter no maximo 50 caracteres");
+          }
+          this.title = title;
+     }
+    
+}
---
+++ src/main/java/br/com/huansantos/todolist/user/UserController.java
@@ -0,0 +1,35 @@
+package br.com.huansantos.todolist.user;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private IUserRepository userRepository;
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null){
+            //mensagem de erro
+            // status code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuario já Existe");
+        }
+
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.OK).body(userCreated);
+    }
+} 
+
